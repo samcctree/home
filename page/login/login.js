7caf32e4fd97545cddddb8a113c0fa95b1400cce
yzhh <835292875@qq.com>
2024-11-06 18:00:40 +0800

    add file

---
+++ page/login/login.js
@@ -0,0 +1,55 @@
+layui.use(['form','layer','jquery'],function(){
+    var form = layui.form,
+        layer = parent.layer === undefined ? layui.layer : top.layer
+        $ = layui.jquery;
+
+    $(".loginBody .seraph").click(function(){
+        layer.msg("这只是做个样式，至于功能，你见过哪个后台能这样登录的？还是老老实实的找管理员去注册吧",{
+            time:5000
+        });
+    })
+
+    //登录按钮
+    form.on("submit(login)",function(data){
+
+        //$(this).text("登录中a").attr("disabled","disabled").addClass("layui-disabled");
+
+        var username = $('#userName').val().trim();
+        var password = $('#password').val().trim();
+
+        var param = {
+            user_name : username,
+            user_passwd:  password
+        };
+
+        $.post("/account_login",param,function(data){
+
+            if(data.code == 200){
+                //layer.msg('登录成功！');
+                location.href = '/';
+            }else{
+                layer.msg(data.msg,{icon: 2});
+            }
+
+        })
+
+        return false;
+    })
+
+    //表单输入效果
+    $(".loginBody .input-item").click(function(e){
+        e.stopPropagation();
+        $(this).addClass("layui-input-focus").find(".layui-input").focus();
+    })
+    $(".loginBody .layui-form-item .layui-input").focus(function(){
+        $(this).parent().addClass("layui-input-focus");
+    })
+    $(".loginBody .layui-form-item .layui-input").blur(function(){
+        $(this).parent().removeClass("layui-input-focus");
+        if($(this).val() != ''){
+            $(this).parent().addClass("layui-input-active");
+        }else{
+            $(this).parent().removeClass("layui-input-active");
+        }
+    })
+})
