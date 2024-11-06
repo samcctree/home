dd336088f9511923dbcfcefe71618797cdc8d315
samcctree <183140828@qq.com>
2024-11-06 16:52:16 +0800

    Add files via upload

---
+++ JsonUrl.java
@@ -0,0 +1,53 @@
+package com.worldscan.shouzhuan.android;
+
+public class JsonUrl {
+	public static final String Version = "1.0.0";
+	public static final String app_secret = "15c95f2e448ff74d778652c2fb91cc3d";//微信AppSecret
+	public static class ShowMsgActivity {
+		public static final String STitle = "showmsg_title";
+		public static final String SMessage = "showmsg_message";
+		public static final String BAThumbData = "showmsg_thumb_data";
+	}
+	public static final String isAudit = "http://www.diandianmall.cn/api/login/isoncheck";//是否在审核
+	public static final String MobileUserFaceURL = "http://weixz.607gg.cn/userface.png";//默认头像
+	public static final String WXLoginURL = "http://www.diandianmall.cn/api/login/wechatlogin";//微信登录
+	public static final String WXGetAccessToken = "https://api.weixin.qq.com/sns/oauth2/access_token?";//获取access_token
+	public static final String WXShareAddNumber = "http://www.diandianmall.cn/api/draw/adddraw";//分享成功增加回调次数
+	public static final String WXShareURL = "http://www.diandianmall.cn/api/geturl/contentgetshare";//微信分享URL
+	public static final String INDEXTabURL = "http://www.diandianmall.cn/api/article";//首页数据分类
+	public static final String SignInDataURL = "http://www.diandianmall.cn/api/login/daysigninfo";//签到信息
+	public static final String SignInURL = "http://www.diandianmall.cn/api/login/daysign";//签到
+	public static final String EnlighteningURL = "http://www.diandianmall.cn/api/pupil";//收徒
+	public static final String MineDataURL = "http://www.diandianmall.cn/api/person";//我的页面数据
+	public static final String YANZHENGMAURL = "http://www.diandianmall.cn/api/login/sendverify";//发送验证码
+	public static final String PhoneLoginURL = "http://www.diandianmall.cn/api/login/index";//手机号码登录
+	public static final String ArticleDetailsURL = "http://www..cn/api/article/articledetail";//文章详情
+	public static final String SharinrulesURL = "http://www.diandianmadiandianmallll.cn/api/static/sharerule";//分享规则解析
+	public static final String RewardRulesURL = "http://www.diandianmall.cn/api/static/rule";//奖励规则解析
+	public static final String StrategiesURL = "http://www.diandianmall.cn/api/static/raiders";//赚钱攻略
+	public static final String HowDiscipleURL = "http://www.diandianmall.cn/api/static/how";//如何收取更多徒弟
+	public static final String WhyDiscipleURL = "http://www.diandianmall.cn/api/static/why";//为什么要收徒
+	public static final String MineDiscipleURL = "http://www.diandianmall.cn/api/pupil/mypupil";//我的徒弟
+	public static final String MineTuSunURL = "http://www.diandianmall.cn/api/pupil/mypupil2";//我的徒孙
+	public static final String FromMasterURL = "http://www.diandianmall.cn/api/static/findteacher";//拜师
+	public static final String MineAnnouncementURL = "http://www.diandianmall.cn/api/message/notice";//系统公告
+	public static final String WechatStatementURL = "http://www.diandianmall.cn/api/message/noticedetail";//微信提现公告
+	public static final String OneWithdrawalURL = "http://www.diandianmall.cn/api/getmoney/";//一元提现
+	public static final String WithdrawalRecordURL = "http://www.diandianmall.cn/api/getmoney/record";//提现记录
+	public static final String BindPhoneURL = "http://www.diandianmall.cn/api/login/bind";//绑定手机号
+	public static final String PublicAttentionURL = "http://www.diandianmall.cn/api/static/guanzhuinfo";//关注公众号流程
+	public static final String WithdrawalYesOrNoURL = "http://www.diandianmall.cn/api/getmoney/getmoney";//提现成功与否
+	public static final String TheNewURL = "http://www.diandianmall.cn/api/static/introduction";//新手入门
+	public static final String IncomeDeatilsURL = "http://www.diandianmall.cn/api/person/incomedetail";//收入明细
+	public static final String ShareTheDetailsURL = "http://www.diandianmall.cn/api/person/shardetail";//分享明细
+	public static final String MessageURL = "http://www.diandianmall.cn/api/message";//消息
+	public static final String ReadSysMessageURL = "http://www.diandianmall.cn/api/message/sysMessageRead";//已阅读系统消息
+	public static final String ReadUserMessageURL = "http://www.diandianmall.cn/api/message/alreadyread";//已阅读私人消息
+	public static final String DelectMessageURL = "http://www.diandianmall.cn/api/message/clear";//清空消息
+	public static final String MessageNumberURL = "http://www.diandianmall.cn/api/message/messageNum";//未读消息数量
+	public static final String LotteryPrizeDataURL = "http://www.diandianmall.cn/api/draw/baseinfo";//抽奖基本信息
+	public static final String LotteryPrizeURL = "http://www.diandianmall.cn/api/draw/index";//抽奖
+	public static final String FeedBackURL = "http://www.diandianmall.cn/api/static/opinion";//意见反馈
+	public static final String UserAgreementURL = "http://www.diandianmall.cn/api/static/agreement";//用户协议
+	public static final String EnlighteningShareURL = "http://www.diandianmall.cn/api/pupil/sharetitle";//分享收徒推广语
+}
